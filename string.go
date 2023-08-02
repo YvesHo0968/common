67ff5c245ff4dcddc87fdd1a1a786ed958cfd1f1
YvesHo <446572531@qq.com>
2023-08-03 00:19:40 +0800

    Added function AddCSlashes()、AddSlashes()、Bin2Hex() and  Chop()

--- string.go
+++ string.go
@@ -1,6 +1,55 @@
 package common
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// AddCSlashes 返回在指定的字符前添加反斜杠的字符串
+func AddCSlashes(str string, characters string) string {
+	escapedStr := ""
+	for _, ch := range str {
+		chStr := string(ch)
+		if strings.ContainsRune(characters, ch) {
+			chStr = "\\" + chStr
+		}
+		escapedStr += chStr
+	}
+	return escapedStr
+}
+
+// AddSlashes 预定义的字符前添加反斜杠的字符串
+func AddSlashes(str string) string {
+	str = strings.ReplaceAll(str, `\`, `\\`)    // 转义反斜杠
+	str = strings.ReplaceAll(str, `'`, `\'`)    // 转义单引号
+	str = strings.ReplaceAll(str, `"`, `\"`)    // 转义双引号
+	str = strings.ReplaceAll(str, "\x00", `\0`) // 转义 NULL 字符
+	return str
+}
+
+// Bin2Hex 把 ASCII 字符的字符串转换为十六进制值
+func Bin2Hex(str string) string {
+	hexStr := ""
+	for _, ch := range str {
+		hexStr += fmt.Sprintf("%x", ch)
+	}
+	return hexStr
+}
+
+// Chop 移除字符串右侧的空白字符或其他预定义字符
+func Chop(str string, charList ...string) string {
+	if len(charList) == 0 {
+		str = strings.TrimRight(str, "\x00")
+		str = strings.TrimRight(str, "\t")
+		str = strings.TrimRight(str, "\n")
+		str = strings.TrimRight(str, "\x0B")
+		str = strings.TrimRight(str, "\r")
+		str = strings.TrimRight(str, " ")
+		return str
+	}
+
+	return strings.TrimRight(str, charList[0])
+}
 
 // StrVal 任意类型转字符串
 func StrVal(data any) string {
--- string_test.go
+++ string_test.go
@@ -5,6 +5,25 @@ import (
 	"testing"
 )
 
+func TestAddCSlashes(t *testing.T) {
+	fmt.Println(AddCSlashes("test sa", "sa"))
+}
+
+func TestAddSlashes(t *testing.T) {
+	fmt.Println(AddSlashes("Who's Peter Griffin?"))
+}
+
+func TestBin2Hex(t *testing.T) {
+	fmt.Println(Bin2Hex("123"))
+}
+
+func TestChop(t *testing.T) {
+	str := "Hello, World!      \t\n"
+	charList := "\t\n"
+	choppedStr := Chop(str, charList)
+	fmt.Println(choppedStr) //
+}
+
 func TestStrVal(t *testing.T) {
 	fmt.Println(StrVal("test"))
 	fmt.Println(StrVal(1))
