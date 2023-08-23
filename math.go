db4827f46f82c4f5de57f8288226578de8b02864
YvesHo <446572531@qq.com>
2023-08-23 22:30:26 +0800

    Added function
    ASin()、ASinH() and ATan()

--- math.go
+++ math.go
@@ -16,3 +16,18 @@ func ACos(num float64) float64 {
 func ACosh(num float64) float64 {
 	return math.Log(num + math.Sqrt(num*num-1.0))
 }
+
+// ASin 返回一个数的反正弦
+func ASin(num float64) float64 {
+	return math.Asin(num)
+}
+
+// ASinH 返回一个数的反双曲正弦
+func ASinH(num float64) float64 {
+	return math.Asinh(num)
+}
+
+// ATan 返回一个数的反正切
+func ATan(num float64) float64 {
+	return math.Atan(num)
+}
--- math_test.go
+++ math_test.go
@@ -16,3 +16,15 @@ func TestACos(t *testing.T) {
 func TestACosh(t *testing.T) {
 	fmt.Println(ACosh(7))
 }
+
+func TestASin(t *testing.T) {
+	fmt.Println(ASin(0.64))
+}
+
+func TestASinH(t *testing.T) {
+	fmt.Println(ASinH(7))
+}
+
+func TestATan(t *testing.T) {
+	fmt.Println(ATan(0.50))
+}
